0cac46f1eaa053bae4d6f7014af8313cb8b971b1
Tran Van Anh <anhva216@gmail.com>
2025-10-04 17:00:56 +0700

    Update Doctor.cpp

--- src/Doctor.cpp
+++ src/Doctor.cpp
@@ -3,16 +3,21 @@
 #include <string>
 using namespace std;
 
-Doctor::Doctor() : Staff() {
-    chuyenKhoa = "";
+Doctor::Doctor() {
+    id = name = DOB = gender = "";
+    phone = 0;
+    chuyenKhoa = ""; 
     namKinhNghiem = 0;
-    bangCap = "";
+    bangCap = 0;
 }
 
-Doctor::Doctor(string id, string name, string DOB, string gender, 
-               int phone, double salary, string chuyenKhoa, int namKinhNghiem, string bangCap) 
-               : Staff(id, name, DOB, gender, phone) 
-{
+Doctor::Doctor(string id, string name, string DOB, string gender, int phone,
+               string chuyenKhoa, int namKinhNghiem, int bangCap) {
+    this->id = id;
+    this->name = name;
+    this->DOB = DOB;
+    this->gender = gender;
+    this->phone = phone;
     this->chuyenKhoa = chuyenKhoa;
     this->namKinhNghiem = namKinhNghiem;
     this->bangCap = bangCap;
@@ -20,28 +25,21 @@ Doctor::Doctor(string id, string name, string DOB, string gender,
 
 void Doctor::nhap() {
     Staff::nhap();
-
+    cout << "enter specialization: ";
     cin.ignore();
-    cout << "Nhap chuyen khoa: ";
     getline(cin, chuyenKhoa);
-    cout << "Nhap so nam kinh nghiem: ";
+    cout << "enter years of experience: ";
     cin >> namKinhNghiem;
-    cin.ignore();
-    cout << "Nhap bang cap: ";
-    getline(cin, bangCap);
+    cout << "enter number of degrees: ";
+    cin >> bangCap;
 }
 
 void Doctor::in() const {
     Staff::in();
-
-    cout << "Chuyen khoa: " << chuyenKhoa << endl;
-    cout << "Nam kinh nghiem: " << namKinhNghiem << endl;
-    cout << "Bang cap: " << bangCap << endl;
-    cout << "Luong: " << tinhLuong() << endl;
+    cout << chuyenKhoa << " " << namKinhNghiem << " " << bangCap << "\n";
 }
 
 double Doctor::tinhLuong() const {
-    double luongCoBan = 8000000; 
+    double luongCoBan = 8000000;
     return luongCoBan + namKinhNghiem * 200000 + bangCap * 500000;
 }
-
