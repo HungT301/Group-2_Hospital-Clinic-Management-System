1c97a4149b742b832757aa04e0de0260d6b53ff0
vanah00 <anhva216@gmail.com>
2025-10-04 16:36:15 +0700

    Update Staff.cpp

--- src/Staff.cpp
+++ src/Staff.cpp
@@ -3,20 +3,24 @@
 #include <iostream>
 using namespace std;
 
-Staff :: void nhap(){
-  cout << "enter the ID:"; cin >> id;
-  cout << "enter the name:"; 
-  cin.ignore();
-  getline(cin, name);
-  cout << "enter Date Of Birth:"; cin >> DOB;
-  cout << "enter the Gender:"; cin >> gender;
-  cout << "enter the phone number:"; cin >> phone;
+void Staff::nhap() {
+    cout << "enter the ID: ";
+    cin >> id;
+    cout << "enter the name: ";
+    cin.ignore();             
+    getline(cin, name);
+    cout << "enter Date Of Birth: ";
+    cin >> DOB;
+    cout << "enter the Gender: ";
+    cin >> gender;
+    cout << "enter the phone number: ";
+    cin >> phone;
 }
 
-Staff : void in() const{
-  cout << id << " " << name << " " << DOB << " " << gender << " " << phone << "\n";
+void Staff::in() const {
+    cout << id << " " << name << " " << DOB << " " << gender << " " << phone << "\n";
 }
 
-Staff : string getID() const{
-  return this->id;
+string Staff::getID() const {
+    return this->id;
 }
