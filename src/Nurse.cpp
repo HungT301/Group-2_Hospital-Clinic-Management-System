9d784dd14b229dbf955d9a60c38210cfff481ee5
Vũ Thu Hằng <11247393@st.neu.edu.vn>
2025-10-07 13:45:48 +0700

    Update Nurse.cpp

--- src/Nurse.cpp
+++ src/Nurse.cpp
@@ -7,21 +7,22 @@ using namespace std;
 Nurse :: Nurse(){
   id = ""; name = ""; DOB =""; gender = "";
   phone=0;
-  caTruc = ""; phongTruc "";
+  shift = ""; oncallroom = "";
 }
 
 void Nurse :: nhap(){
   Staff::nhap();
-  cout << "enter the on-call:";
+  cout << "enter the shift:";
   cin.ignore();
-  cin >> caTruc;
-  cout << "enter thr duty room:"; cin >> phongTruc;
+  cin >> shift;
+  cout << "enter the on-call room:"; cin >> oncallroom;
 }
 
 void Nurse :: in() const {
   Staff :: in();
-  cout << caTruc << " " << phongTruc << "\n";
+  cout << shift << " " << oncallroom << "\n";
 }
+
 
 
 
