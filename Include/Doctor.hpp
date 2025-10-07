7109bfe264a3df94df254c21ec765a56c6bb829e
Vũ Thu Hằng <11247393@st.neu.edu.vn>
2025-10-07 12:43:23 +0700

    Update Doctor.hpp

--- Include/Doctor.hpp
+++ Include/Doctor.hpp
@@ -7,16 +7,13 @@ using namespace std;
 class Doctor : public Staff {
 private:
 	string chuyenKhoa;
-	int namKinhNghiem;
-	string bangCap;
+	string schedule; // chi ghi cac thu lam viec (input : Thu 2, 3, 4, 5 - Sang)
 
 public:
 	Doctor();
-	Doctor(string, string, string, string, int, double, string , int, string);
 
 	void nhap();
 	void in() const;
-	double tinhLuong() const;
 };
 
 #endif
