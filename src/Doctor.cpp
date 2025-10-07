cd7a3bc754853f38e61cb778f5fc0b066aacc101
Tran Van Anh <anhva216@gmail.com>
2025-10-07 16:16:01 +0700

    Update Doctor.cpp

--- src/Doctor.cpp
+++ src/Doctor.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <limits> 
 using namespace std;
 
 Doctor::Doctor() {}
@@ -10,11 +12,27 @@ void Doctor::nhap() {
     Staff::nhap();
     cout << "Enter specialization: ";
     getline(cin, specialization);
+
+    int n;
+    cout << "Enter number of working shifts: ";
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    schedules.resize(n);
+    for (int i = 0; i < n; ++i) {
+        cout << "Enter schedule " << i + 1 << " (e.g. Thu 2 - Sang): ";
+        getline(cin, schedules[i]);
+    }
 }
 
 void Doctor::in() const {
     Staff::in();
     cout << "Specialization: " << specialization << endl;
+    cout << "Schedules: ";
+    for (const auto &s : schedules) {
+        cout << s << " ";
+    }
+    cout << endl;
 }
 
 
