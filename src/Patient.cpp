ec06c67f912b8c035c16c203b3124db13af7d8b5
tnahoaa <hoathanhhh1@gmail.com>
2025-10-07 23:14:59 +0700

    Update Patient.cpp

--- src/Patient.cpp
+++ src/Patient.cpp
@@ -1,44 +1,25 @@
 #include "Patient.hpp"
 
 #include <iostream>
+#include <string>
+#include <limits>
 using namespace std;
 
 Patient::Patient() {}
 
 void Patient::nhap() {
-    cout << "Enter patient ID: ";
-    getline(cin, id);
-
-    cout << "Enter name: ";
-    getline(cin, name);
-
-    cout << "Enter date of birth: ";
-    getline(cin, DOB);
-
-    cout << "Enter phone: ";
-    getline(cin, phone);
-
-    cout << "Enter gender: ";
-    getline(cin, gender);
-
+    Person::nhap();
     cout << "Enter symptoms: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, symptoms);
 
     cout << "Enter diagnosis: ";
     getline(cin, diagnosis);
 }
 
-void Patient::in() {
-    cout << "ID: " << id << endl;
-    cout << "Name: " << name << endl;
-    cout << "Date of birth: " << DOB << endl;
-    cout << "Phone: " << phone << endl;
-    cout << "Gender: " << gender << endl;
-    cout << "Symptoms: " << symptoms << endl;
-    cout << "Diagnosis: " << diagnosis << endl;
+void Patient::in() const {
+    Person::in();
+    cout << " " << symptoms << " " << diagnosis;
 }
 
-string Patient::getID() const {
-    return id;
-}
 
