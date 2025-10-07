ee05eb6d97c98e412e2e8138035b0649a4066174
Vũ Thu Hằng <11247393@st.neu.edu.vn>
2025-10-07 13:55:56 +0700

    Update Nurse.cpp

--- src/Nurse.cpp
+++ src/Nurse.cpp
@@ -1,26 +1,53 @@
 #include "Nurse.hpp"
 
 #include <iostream>
+#include <vector>
 #include <string>
 using namespace std;
 
 Nurse :: Nurse(){
-  id = ""; name = ""; DOB =""; gender = "";
-  phone=0;
-  shift = ""; oncallroom = "";
+ // cái này constructor t chưa biết sửa như nào m sửa cho t  nhá!!!
 }
 
 void Nurse :: nhap(){
   Staff::nhap();
-  cout << "enter the shift:";
-  cin.ignore();
-  cin >> shift;
-  cout << "enter the on-call room:"; cin >> oncallroom;
+
+  int numShifts;
+    cout << "Nhap so luong ca lam: ";
+    cin >> numShifts;
+    cin.ignore(); 
+
+    for (int i = 0; i < numShifts; i++) {
+        string shift;
+        cout << "  Nhap ca lam thu " << i + 1 << ": ";
+        getline(cin, shift);
+        shifts.push_back(shift);
+    }
+
+    int numRooms;
+    cout << "Nhap so luong phong truc: ";
+    cin >> numRooms;
+    cin.ignore();
+
+    for (int i = 0; i < numRooms; i++) {
+        string room;
+        cout << "  Nhap phong truc thu " << i + 1 << ": ";
+        getline(cin, room);
+        onCallRooms.push_back(room);
+    }
 }
 
 void Nurse :: in() const {
   Staff :: in();
-  cout << shift << " " << oncallroom << "\n";
+  cout << "Cac ca lam:\n";
+  for (const string& s : shifts){
+    cout << "  - " << s << endl;
+  };
+  
+  cout << "Phong truc:\n";
+  for (const string& r : onCallRooms){
+        cout << "  - " << r << endl;
+  }
 }
 
 
