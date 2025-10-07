c47c51b145f75697a115d92217b92c05095b17cb
tnahoaa <hoathanhhh1@gmail.com>
2025-10-07 15:32:16 +0700

    Update Patient.cpp

--- src/Patient.cpp
+++ src/Patient.cpp
@@ -14,5 +14,34 @@ Patient::Patient(string id, string ten, string ngaysinh, string sdt,
     this->trieuChung = trieuChung;
     this->chanDoan = chanDoan;
 }
+void Patient::nhap() {
+    cout << "Enter patient ID: ";
+    getline(cin, id);
+    cout << "Enter name: ";
+    getline(cin, ten);
+    cout << "Enter date of birth: ";
+    getline(cin, ngaysinh);
+    cout << "Enter phone number: ";
+    getline(cin, sdt);
+    cout << "Enter gender: ";
+    getline(cin, gioiTinh);
+    cout << "Enter symptoms: ";
+    getline(cin, trieuChung);
+    cout << "Enter diagnosis: ";
+    getline(cin, chanDoan);
+}
 
+void Patient::in() const {
+    cout << "\n--- PATIENT INFORMATION ---\n";
+    cout << "ID: " << id << endl;
+    cout << "Name: " << ten << endl;
+    cout << "Date of birth: " << ngaysinh << endl;
+    cout << "Phone: " << sdt << endl;
+    cout << "Gender: " << gioiTinh << endl;
+    cout << "Symptoms: " << trieuChung << endl;
+    cout << "Diagnosis: " << chanDoan << endl;
+}
+string Patient::getID() const {
+    return id;
+}
 
