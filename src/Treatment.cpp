c5086fe9168fb7d5c7d671cb6e57ef65e8d181d0
Nguyễn Tuấn Anh <11247382@st.neu.edu.vn>
2025-10-08 19:45:47 +0700

    Update Treatment.cpp

--- src/Treatment.cpp
+++ src/Treatment.cpp
@@ -1,33 +1,38 @@
-#include "Treatment.hpp"
+#include "treatment.hpp"
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-Treatment::Treatment() {}
+Treatment::Treatment() {
+    id = "";
+    patientId = "";
+    doctorId = "";
+    moTa = "";
+    cost = 0.0;
+}
 
 void Treatment::nhap() {
-    cout << "Enter Treatment ID: ";
+    cout << "Enter treatment ID: ";
     getline(cin, id);
-    cout << "Enter Patient ID: ";
+    cout << "Enter patient ID: ";
     getline(cin, patientId);
-    cout << "Enter Doctor ID: ";
+    cout << "Enter doctor ID: ";
     getline(cin, doctorId);
-    cout << "Enter Description: ";
+    cout << "Enter treatment description: ";
     getline(cin, moTa);
-    cout << "Enter Cost: ";
-    cin >> chiPhi;
+    cout << "Enter cost: ";
+    cin >> cost;
     cin.ignore();
 }
 
 void Treatment::in() const {
-    cout << " " << id << endl;
-    cout << " " << patientId << endl;
-    cout << " " << doctorId << endl;
-    cout << " " << moTa << endl;
-    cout << " " << chiPhi << endl;
+    cout << left << setw(20) << " " << id << endl;
+    cout << left << setw(20) << " " << patientId << endl;
+    cout << left << setw(20) << " " << doctorId << endl;
+    cout << left << setw(20) << " " << moTa << endl;
+    cout << left << setw(20) << " " << fixed << setprecision(2) << cost << endl;
 }
 
-void Treatment::tinhChiPhi() const {
-    cout << " " << chiPhi << endl;
+void Treatment::computeCost() const {
+    cout << " " << fixed << setprecision(2) << cost << endl;
 }
-
-
