29d464bc65815c62a8b91fb007e61f80aadbaab2
Hung Trinh <alter3012006@gmail.com>
2025-10-11 20:07:53 +0700

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "Person.hpp"
+#include "Staff.hpp"
+#include "Doctor.hpp"
+#include "Nurse.hpp"
+#include "Patient.hpp"
+#include "Appointment.hpp"
+#include "AppointmentType.hpp"
+#include "Treatment.hpp"
+#include "TreatmentType.hpp"
+using namespace std;
+
+// Menu
+void menuDoctor(vector<Doctor>& doctors);
+void menuNurse(vector<Nurse>& nurses);
+void menuPatient(vector<Patient>& patients);
+void menuAppointment(vector<Appointment*>& appointments);
+void menuTreatment(vector<Treatment*>& treatments);
+
+// Main
+int main() {
+
+    vector<Doctor> doctors;
+    vector<Nurse> nurses;
+    vector<Patient> patients;
+    vector<Appointment*> appointments;
+    vector<Treatment*> treatments;
+
+    int choice;
+    do {
+        cout << "\n===== HOSPITAL MANAGEMENT SYSTEM =====\n";
+        cout << "1. Manage Doctor\n";
+        cout << "2. Manage Nurse\n";
+        cout << "3. Manage Patient\n";
+        cout << "4. Manage Appointment\n";
+        cout << "5. Manage Treatment\n";
+        cout << "0. Exit.\n";
+        cout << "Choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+            case 1: menuDoctor(doctors); break;
+            case 2: menuNurse(nurses); break;
+            case 3: menuPatient(patients); break;
+            case 4: menuAppointment(appointments); break;
+            case 5: menuTreatment(treatments); break;
+            case 0: cout << "Exit program.\n"; break;
+            default: cout << "Invalid choice!\n";
+        }
+
+    } while (choice != 0);
+
+    for (auto p : appointments) delete p;
+    for (auto t : treatments) delete t;
+
+    return 0;
+}
+
+// Menu deploy
+
+// Doctor
+void menuDoctor(vector<Doctor>& doctors) {}
+
+// Nurse
+void menuNurse(vector<Nurse>& nurses) {}
+
+// Patient
+void menuPatient(vector<Patient>& patient) {}
+
+// Appointment
+void menuAppointment(vector<Appointment*>& appointments) {}
+
+//Treatment
+void menuTreatment(vector<Treatment*>& treatments) {}
