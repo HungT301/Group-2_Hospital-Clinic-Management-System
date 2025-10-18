aca6a2dcb54e069f2d660757a60a3721e7a1e98b
Tran Van Anh <anhva216@gmail.com>
2025-10-19 00:51:57 +0700

    Update Doctor.cpp

--- src/Doctor.cpp
+++ src/Doctor.cpp
@@ -1,38 +1,58 @@
 #include "Doctor.hpp"
 
 #include <iostream>
-#include <iomanip> 
+#include <iomanip>
 using namespace std;
 
 Doctor::Doctor() {}
 
 void Doctor::nhap() {
     Staff::nhap();
+
     cout << "Enter specialization: ";
     getline(cin, specialization);
 
-    int n;
-    cout << "Enter number of working shifts: ";
-    cin >> n;
+    int numSchedules;
+    cout << "Enter the number of schedules: ";
+    cin >> numSchedules;
     cin.ignore();
 
-    schedules.resize(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Enter schedule " << i + 1 << " (e.g. Thu 2 - Sang): ";
-        getline(cin, schedules[i]);
+    schedules.clear();
+    for (int i = 0; i < numSchedules; i++) {
+        string schedule;
+        cout << "Schedule " << i + 1 << " (e.g. Mon: Morning): ";
+        getline(cin, schedule);
+        schedules.push_back(schedule);
     }
 }
 
 void Doctor::in() const {
-    Staff::in();
+    cout << left
+         << setw(10) << staffID
+         << setw(20) << name
+         << setw(15) << dob
+         << setw(15) << gender
+         << setw(15) << phone
+         << setw(20) << specialization
+         << endl;
+}
 
-    string allSchedules;
-    for (int i = 0; i < schedules.size(); i++) {
-        allSchedules += schedules[i];
-        if (i != schedules.size() - 1) allSchedules += ", ";
+void Doctor::display() const {
+    cout << left
+         << setw(10) << staffID
+         << setw(20) << name
+         << setw(20) << specialization
+         << endl;
+
+    if (schedules.empty()) {
+        cout << "   No schedules available.\n";
+    } else {
+        cout << "   Schedules: ";
+        for (int i = 0; i < schedules.size(); i++) {
+            cout << schedules[i];
+            if (i != schedules.size() - 1) cout << ", ";
+        }
+        cout << endl;
     }
-
-     cout << left
-         << setw(18) << specialization
-         << setw(40) << allSchedules << endl;
+    cout << string(60, '-') << endl;
 }
