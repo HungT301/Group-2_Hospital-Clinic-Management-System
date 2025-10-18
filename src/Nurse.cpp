c5f38414157b74eb46ce7c4aa6bc7fccbef74334
Thu Hang Vu <11247393@st.neu.edu.vn>
2025-10-19 00:05:57 +0700

    Update Nurse.cpp

--- src/Nurse.cpp
+++ src/Nurse.cpp
@@ -10,25 +10,25 @@ void Nurse :: nhap(){
   Staff::nhap();
 
   int numShifts;
-    cout << " the number of Shifts: ";
+    cout << "Enter the number of Shifts: ";
     cin >> numShifts;
     cin.ignore();
   
   for (int i = 0; i < numShifts; i++) {
     string shift;
-    cout << "Shift" << i + 1 << ": ";
+    cout << "Shift " << i + 1 << ": ";
     getline(cin, shift);
     shifts.push_back(shift);
 
     int numRooms;
-    cout << " the number of Oncall Rooms: ";
+    cout << "Enter the number of Oncall Rooms: ";
     cin >> numRooms;
     cin.ignore();
 
     vector<string> rooms;
     for (int j = 0; j < numRooms; j++) {
         string room;
-        cout << "   " << j + 1 << ": ";
+        cout << "Oncall Room " << j + 1 << ": ";
         getline(cin, room);
         rooms.push_back(room);
       }
@@ -60,6 +60,7 @@ void Nurse :: in() const {
          << setw(25) << allShifts
          << setw(50) << allRooms << endl;
 }
+
 
 
 
